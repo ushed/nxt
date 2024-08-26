2d9a95122ec5f5f471a80c4eb35882e73a95e02b
ushed <137984028+ushed@users.noreply.github.com>
2024-08-26 23:40:28 +0900

    new2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
+import { createMDX } from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ページとして認識するファイルの拡張子を指定
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
-  // プロジェクトを静的エクスポートする設定
   output: "export",
 };
 
-const withMDX = createMDX({
-  // ここに必要なMarkdownプラグインを追加可能
-});
+const withMDX = createMDX({});
 
-// MDX設定とNext.js設定をマージしてエクスポート
 export default withMDX(nextConfig);
