9421c9b25e97a3a078b59b48b521aff47f133e51
ushed <137984028+ushed@users.noreply.github.com>
2024-08-26 23:34:27 +0900

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ページとして認識するファイルの拡張子を指定
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
+  // プロジェクトを静的エクスポートする設定
   output: "export",
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // ここに必要なMarkdownプラグインを追加可能
+});
+
+// MDX設定とNext.js設定をマージしてエクスポート
+export default withMDX(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <main id="main">
       <picture>
         <Image
-          src="/public/favicon/kuma.jpg"
+          src="/favicon/kuma.jpg"
           alt="kumamoto"
           width={500}
           height={500}
@@ -27,19 +27,81 @@ export default async function Home() {
           <li>エンジニアを目指してます</li>
         </ul>
         <p>
-          基本月１更新になると思います
-          <br />
-          <Link href="/creation">
-            <span>Creations</span>
-          </Link>
-          では創作物、
-          <Link href="/blog">
+          基本的に月1で何かをアップする予定しています。
+          <br />「
+          <a href="/creation">
+            <span>creation</span>
+          </a>
+          」 では創作物を紹介し、「
+          <a href="/blog">
             <span>Blog</span>
-          </Link>
-          では何かの動機になれば感覚で書いていくつもりです
+          </a>
+          」 では新しいことに挑戦するためのモチベーションを維持するため
+          の記録を綴っていきます。
           <br />
-          これらを見ていて、皆さんの中にもチャレンジしてみたいことができたら、ぜひ自分のペースでやってみてください！！
+          このサイトを通じて、皆さんも新しいチャレンジを始めるきっかけになれば嬉しいです。
+          自分のペースで、楽しんで取り組んでみてください!!
         </p>
+      </section>
+
+      <section id="creation" className="wrapper">
+        <h2 className="sec-title">
+          Crea<span>tions</span>
+        </h2>
+        <ul>
+          <li>
+            <a href="/creationDetail/1">
+              <img src="/favicon/kuma.jpg" alt="kumamoto1" />
+              <div>
+                <h3>2004/04/04</h3>
+                <p>
+                  あああああああああああああああああああああああああああああああああああああ
+                </p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/creationDetail/1">
+              <img src="/favicon/kuma.jpg" alt="kumamoto2" />
+              <div>
+                <h3>2004/04/04</h3>
+                <p>
+                  あああああああああああああああああああああああああああああああああああああ
+                </p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <a href="/creation" className="btn_a">
+          <div className="btn">
+            <div className="btn_txt">more creation</div>
+          </div>
+        </a>
+      </section>
+
+      <section id="blog" className="wrapper">
+        <h2 className="sec-title">
+          Bl<span>og</span>
+        </h2>
+        <dl>
+          <a href="/blogDetail/3">
+            <dt>2024/08/21</dt>
+            <dd>振り出しに戻ってからの〜</dd>
+          </a>
+          <a href="/blogDetail/2">
+            <dt>2024/08/12</dt>
+            <dd>個人サイト無理ゲー</dd>
+          </a>
+          <a href="/blogDetail/1">
+            <dt>2024/07/20</dt>
+            <dd>何となくやっていく</dd>
+          </a>
+        </dl>
+        <a href="/blog" className="btn_a">
+          <div className="btn">
+            <div className="btn_txt">more blog</div>
+          </div>
+        </a>
       </section>
     </main>
   );
